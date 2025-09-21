b75fd173c5a74bea952d38b2ffaa1837dc9b9f8e
Gmmba <tim0fej10065@gmail.com>
2025-09-21 17:12:26 +0700

    Добавил class Driver

--- Visual_Programming/src/Main.kt
+++ Visual_Programming/src/Main.kt
@@ -3,44 +3,69 @@ import kotlin.math.sin
 import kotlin.math.PI
 import kotlin.random.Random
 
-class Human(
+open class Human(
     var fio: String,
     var age: Int,
     var speed: Int
 ) {
-    private var x: Double = 0.0
-    private var y: Double = 0.0
-    private val random = Random.Default
+    protected var x: Double = 0.0
+    protected var y: Double = 0.0
+    protected val random = Random.Default
 
-    fun move() {
-        val alpha = 2 * PI * random.nextDouble()
+    open fun move() {
+        val thread = Thread {
+            repeat(5) { i ->
+                val alpha = 2 * PI * random.nextDouble()
+                x += speed * cos(alpha)
+                y += speed * sin(alpha)
 
-        // перемещаемся по направлению угла
-        x += speed * cos(alpha)
-        y += speed * sin(alpha)
+                log("$fio (Human) шагает в ($x, $y) [секунда ${i + 1}]")
+                Thread.sleep(1000)
+            }
+        }
+        thread.start()
+    }
 
-        println("$fio переместился в ($x, $y)")
+    @Synchronized
+    protected fun log(msg: String) {
+        println(msg)
+    }
+}
+
+class Driver(
+    fio: String,
+    age: Int,
+    speed: Int
+) : Human(fio, age, speed) {
+
+    private var direction: Double = 2 * PI * random.nextDouble()
+
+    override fun move() {
+        val thread = Thread {
+            repeat(5) { i ->
+                x += speed * cos(direction)
+                y += speed * sin(direction)
+
+                log("$fio (Driver) едет в ($x, $y) [секунда ${i + 1}]")
+                Thread.sleep(1000)
+            }
+        }
+        thread.start()
     }
 }
 
 fun main() {
-    val humans = arrayOf(
+    val humans = listOf(
         Human("Петров Т.И", 19, 10),
         Human("Иванов К.П", 9, 7),
         Human("Смирнов Р.Е", 29, 12),
-        Human("Кузнецов Г.Н", 30, 9),
-        Human("Волков Л.Г", 39, 16),
-        Human("Зайцев В.В", 21, 11),
-        Human("Козлов Ш.Л", 35, 13),
-        Human("Тарасов Е.Е", 16, 17),
-        Human("Титов Т.Т", 80, 6),
-        Human("Куликов Л.О", 59, 8)
+        Human("Кузнецов Г.Н", 30, 9)
     )
+    val driver = Driver("Сидоров А.В", 40, 20)
 
-    repeat(5) { i ->
-        println("Секунда ${i + 1}")
-        humans.forEach { it.move() }
-        println()
-        Thread.sleep(1000)
-    }
+    val all = humans + driver
+
+    all.forEach { it.move() }
+
+    Thread.sleep(6000)
 }
