43431afaa49578833da752a4a36c68585513ce09
Gmmba <tim0fej10065@gmail.com>
2026-04-03 15:58:34 +0700

    lab 11-12

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -84,7 +84,7 @@ void run_server(LocationData* data) {
     zmq::context_t ctx;
     zmq::socket_t sock(ctx, zmq::socket_type::rep);
     sock.bind("tcp://*:5555");
-    std::cout << "Сервер запущен на порту 5555\n";
+    std::cout << " Сервер запущен на порту 5555\n";
     
     while (true) {
         try {
